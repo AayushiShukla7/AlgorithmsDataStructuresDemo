2bbc9f1f806011e6c493c34b6bf8b1dd3808023c
Aayushi Shukla <AayushiShukla27@gmail.com>
2022-11-06 21:18:53 -0400

    First commit

---
+++ AlgorithmsDataStructuresConsoleApp/ADT/Pokemon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructuresConsoleApp.ADT
+{
+    public class Pokemon
+    {
+        // Representation
+        public string Name { get; set; }
+        public int Exp { get; set; }
+        
+        // Operation
+        public bool IsPokemon()
+        {
+            return true;
+        }
+    }
+}
